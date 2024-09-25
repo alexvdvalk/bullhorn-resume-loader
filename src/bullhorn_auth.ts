4c73451130d98f27d41f6e38a1ffbf28691d980d
Alex van der Valk <21194068+alexvdvalk@users.noreply.github.com>
2024-09-25 23:02:51 +0100

    add timeout on auth. gracefull exit

--- src/bullhorn_auth.ts
+++ src/bullhorn_auth.ts
@@ -4,20 +4,28 @@ import { type LoginResponse } from "./interfaces"
 
 export const login = async (username: string, password: string) => {
 
-  const { data } = await axios.get<LoginResponse>('https://universal.bullhornstaffing.com/universal-login/session/login', {
-    params: {
-      username,
-      password
-    }
-  })
-  const rest = data.sessions.find(i => i.name === "rest")
-  console.log(`Logged into corporation ${data.identity.corporationName}`)
-  return axios.create({
-    baseURL: rest?.value.endpoint,
-    params: {
-      "BhRestToken": rest?.value.token
-    }
-  })
+  try {
+
+    const { data } = await axios.get<LoginResponse>('https://universal.bullhornstaffing.com/universal-login/session/login', {
+      timeout: 5000,
+      params: {
+        username,
+        password
+      }
+    })
+    const rest = data.sessions.find(i => i.name === "rest")
+    console.log(`Logged into corporation ${data.identity.corporationName}`)
+    return axios.create({
+      baseURL: rest?.value.endpoint,
+      params: {
+        "BhRestToken": rest?.value.token
+      }
+    })
+  } catch {
+    console.error('Unable to login to Bullhorn. Please check your credentials and try again.')
+    process.exit(1);
+  }
+
 
 };
 
